aa159fce87a174838a1839a8e54ead90a0b9a966
Sanjay405 <58900006+Sanjay405@users.noreply.github.com>
2021-10-25 19:24:54 +0530

    add.java succesfully addded

---
+++ Add.java
@@ -0,0 +1,13 @@
+public class Add {
+
+   public static void main{
+          
+         public int addition( int a, int b){
+            int sum = a+b ;
+            return sum;
+          }
+     }
+}
+                
+            
+            
