290e031306be1d200dd47c3372f9dfeaa91ff787
Sanjay405 <58900006+Sanjay405@users.noreply.github.com>
2021-10-25 19:38:22 +0530

    done process

---
+++ App.java
@@ -0,0 +1,12 @@
+public class App
+  {
+      public static void main(String...args)
+         {
+             Add add = new Add();
+           
+             int addition  =  add.Addition();
+            
+             System.out.println(addition);
+           
+            }
+    }
